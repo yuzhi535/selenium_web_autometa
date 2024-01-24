b6e91ad6f2d1828694f2b57c5693490808a30329
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2024-01-24 14:42:29 +0530

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from extract import take_screenshot
+from PIL import Image
+
+def main():
+    st.title("Website Visualizer")
+    
+    # Get website URL from user input
+    url = st.text_input("Enter a URL:", "")
+    if st.button("Proceed"):
+        if not url:
+            st.warning("URL is empty.")
+        else:
+            visualize(url)
+  
+
+def visualize(url):  
+    try:
+    # Fetch and display the website content
+        with st.spinner("loading website data ..."):
+            # innerHTML = get_innerHTML(url)
+            innerHTML = take_screenshot(url)
+            st.subheader("Website preview:")
+            if innerHTML:
+                st.Image(innerHTML)
+            else:
+                st.error("Error: empty html")
+    
+    except Exception as e:
+        st.error(f"Error: {e}")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ extract.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from PIL import Image
+
+def take_screenshot(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    try:
+        wd = webdriver.Chrome(options=options)
+        wd.set_window_size(1080, 720)  # Adjust the window size here
+        wd.get(url)
+        wd.implicitly_wait(10)
+        screenshot = wd.get_screenshot_as_png()
+    except WebDriverException as e:
+        return Image.new('RGB', (1, 1))
+    finally:
+        if wd:
+            wd.quit()
+
+    return Image.open(BytesIO(screenshot))
